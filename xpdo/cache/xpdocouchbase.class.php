633ea21de993603904a7842e1fbfbb96285a9d00
Jason Coward <jason@opengeek.com>
2016-03-09 16:04:11 -0700

    Add xPDOCouchBase as xPDOCache derivative implementation

---
+++ xpdo/cache/xpdocouchbase.class.php
@@ -0,0 +1,99 @@
+<?php
+/*
+ * Copyright 2010-2015 by MODX, LLC.
+ *
+ * This file is part of xPDO.
+ *
+ * xPDO is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * xPDO is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * xPDO; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/**
+ * Provides a couchbase-powered xPDOCache implementation.
+ *
+ * This requires the couchbase extension for PHP.
+ *
+ * @package xpdo
+ * @subpackage cache
+ */
+class xPDOCouchBase extends xPDOCache {
+    /** @var CouchbaseCluster */
+    protected $couchbase = null;
+    /** @var CouchbaseBucket */
+    protected $bucket;
+
+    public function __construct(& $xpdo, $options = array()) {
+        parent :: __construct($xpdo, $options);
+        if (class_exists('CouchbaseCluster', true)) {
+            $dsn = $this->getOption($this->key . '_couchbase_dsn', $options, $this->getOption('couchbase_dsn', $options, '127.0.0.1'));
+            $username = $this->getOption($this->key . '_couchbase_username', $options, $this->getOption('couchbase_username', $options, ''));
+            $password = $this->getOption($this->key . '_couchbase_password', $options, $this->getOption('couchbase_password', $options, ''));
+
+            $this->couchbase = new CouchbaseCluster($dsn, $username, $password);
+            $bucketName = $this->getOption($this->key . '_couchbase_bucket', $options, $this->key);
+            $this->bucket = $this->couchbase->openBucket($this->bucket, $this->getOption($this->key . '_couchbase_bucket_password', $options, ''));
+            if ($this->bucket instanceof CouchbaseBucket) {
+                $this->initialized = true;
+            } else {
+                $this->couchbase = null;
+                $this->bucket = null;
+                $this->xpdo->log(xPDO::LOG_LEVEL_ERROR, "xPDOCouchBase[{$this->key}]: Error creating couchbase provider for server: {$dsn}");
+            }
+        } else {
+            $this->xpdo->log(xPDO::LOG_LEVEL_ERROR, "xPDOCouchBase[{$this->key}]: Error creating couchbase provider; xPDOCouchBase requires the PHP couchbase extension.");
+        }
+    }
+
+    public function add($key, $var, $expire= 0, $options= array()) {
+        if ($expire > 0) {
+            $options['expiry'] = $expire;
+        }
+        $added= $this->bucket->insert($this->getCacheKey($key), $var, $options);
+        return (bool)$added;
+    }
+
+    public function set($key, $var, $expire= 0, $options= array()) {
+        if ($expire > 0) {
+            $options['expiry'] = $expire;
+        }
+        $set= $this->bucket->upsert($this->getCacheKey($key), $var, $options);
+        return $set;
+    }
+
+    public function replace($key, $var, $expire= 0, $options= array()) {
+        if ($expire > 0) {
+            $options['expiry'] = $expire;
+        }
+        $replaced= $this->bucket->replace($this->getCacheKey($key), $var, $options);
+        return $replaced;
+    }
+
+    public function delete($key, $options= array()) {
+        if (!isset($options['multiple_object_delete']) || empty($options['multiple_object_delete'])) {
+            $deleted= $this->bucket->remove($this->getCacheKey($key), $options);
+        } else {
+            $this->bucket->manager()->flush();
+            $deleted = true;
+        }
+        return $deleted;
+    }
+
+    public function get($key, $options= array()) {
+        return $this->bucket->get($this->getCacheKey($key), $options);
+    }
+
+    public function flush($options= array()) {
+        $this->bucket->manager()->flush();
+        return true;
+    }
+}
